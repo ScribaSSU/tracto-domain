35d433c0b470a66103d925c7abed59742cc8a873
Dzeru <diana.mashkina.00@gmail.com>
2019-10-03 20:26:54 +0400

    return TractDomainApplication class for Jitpack compatibility

---
+++ src/main/java/com/scribassu/tracto/domain/TractoDomainApplication.java
@@ -0,0 +1,12 @@
+package com.scribassu.tracto.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TractoDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TractoDomainApplication.class, args);
+    }
+}
