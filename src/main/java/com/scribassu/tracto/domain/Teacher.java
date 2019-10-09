c497fd8fb71d471099458220d4248faecfdf78af
Dzeru <diana.mashkina.00@gmail.com>
2019-10-09 17:14:46 +0400

    add javax.persistence and id annotations

--- src/main/java/com/scribassu/tracto/domain/Teacher.java
+++ src/main/java/com/scribassu/tracto/domain/Teacher.java
@@ -3,8 +3,16 @@ package com.scribassu.tracto.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 public class Teacher {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
 
 }
--- src/main/java/com/scribassu/tracto/domain/TeacherLesson.java
+++ src/main/java/com/scribassu/tracto/domain/TeacherLesson.java
@@ -3,7 +3,16 @@ package com.scribassu.tracto.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 public class TeacherLesson {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
 }
