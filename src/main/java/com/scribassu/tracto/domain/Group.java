fb21d64d1429ed0763f63cdac58b4bd333af8e23
AlexandraMik <mik8856488@gmail.com>
2019-10-15 16:21:27 +0400

    Add id for Group

--- src/main/java/com/scribassu/tracto/domain/Group.java
+++ src/main/java/com/scribassu/tracto/domain/Group.java
@@ -3,10 +3,17 @@ package com.scribassu.tracto.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 public class Group {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String groupNumber;
     private Department department;
     private GroupType groupType;
